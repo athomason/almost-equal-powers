c6f7b3368b099b741244dbce2aca3ec65ade32f1
Adam Thomason <ad@mthomason.net>
2014-03-03 16:29:05 -0800

    Parallelize work

--- almost-equal-powers.go
+++ almost-equal-powers.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"math"
 	"math/big"
+	"runtime"
+	"sync"
 )
 
 func main() {
@@ -42,84 +44,103 @@ func main() {
 		powers = append(powers, big.NewInt(int64(i)))
 	}
 
-	type tuple struct {
-		base, power *big.Int
-	}
-	hits, misses := 0, 0
-	cache := make(map[tuple]*big.Int)
-	pow := func(base, power *big.Int) *big.Int {
-		t := tuple{base, power}
-		if value, exists := cache[t]; exists {
-			hits++
-			return value
-		}
-		misses++
-		value := big.NewInt(0).Exp(base, power, nil)
-		cache[t] = value
-		return value
-	}
-
 	oneInt := big.NewInt(1)
 
-	for _, base1 := range bases {
-		log1 := math.Log(float64(base1.Int64()))
-		for _, base2 := range bases {
-			// don't duplicate work
-			if base1.Cmp(base2) >= 0 {
-				continue
+	nThreads := runtime.GOMAXPROCS(-1)
+
+	var wg sync.WaitGroup
+	compute := func(id int) {
+		defer wg.Done()
+
+		type tuple struct {
+			base, power *big.Int
+		}
+		hits, misses := 0, 0
+		cache := make(map[tuple]*big.Int)
+		pow := func(base, power *big.Int) *big.Int {
+			t := tuple{base, power}
+			if value, exists := cache[t]; exists {
+				hits++
+				return value
 			}
+			misses++
+			value := big.NewInt(0).Exp(base, power, nil)
+			cache[t] = value
+			return value
+		}
 
-			// only consider relative primes
-			var gcd big.Int
-			gcd.GCD(nil, nil, base1, base2)
-			if gcd.Cmp(oneInt) != 0 {
-				continue
-			}
-
-			for _, power1 := range powers {
-				big1 := pow(base1, power1)
-				log2 := math.Log(float64(base2.Int64()))
-
-				power2 := float64(power1.Int64()) * log1 / log2
-				power2Low := big.NewInt(int64(math.Floor(power2)))
-				power2High := big.NewInt(int64(math.Ceil(power2)))
-
-				big2Low := pow(base2, power2Low)
-				big2High := pow(base2, power2High)
-
-				if verbose {
-					fmt.Printf("%v**%v (%v) < %v**%v (%v) < %v**%v (%v)\n",
-						base2, power2Low, big2Low,
-						base1, power1, big1,
-						base2, power2High, big2High,
-					)
+		var n int
+		for _, base1 := range bases {
+			log1 := math.Log(float64(base1.Int64()))
+			for _, base2 := range bases {
+				// don't duplicate work
+				if base1.Cmp(base2) >= 0 {
+					continue
 				}
 
-				ratioLow := big.NewRat(0, 1).SetFrac(big1, big2Low)
-				ratioHigh := big.NewRat(0, 1).SetFrac(big1, big2High)
-
-				if ratioLow.Cmp(lowTolerance) < 0 {
-					rat, _ := ratioLow.Float64()
-					fmt.Printf("%v**%v=%v ~~ %v**%v=%v (%.6f%%)\n",
-						base1, power1, big1,
-						base2, power2Low, big2Low,
-						100*(rat-1),
-					)
+				// only consider relative primes
+				var gcd big.Int
+				gcd.GCD(nil, nil, base1, base2)
+				if gcd.Cmp(oneInt) != 0 {
+					continue
 				}
 
-				if ratioHigh.Cmp(highTolerance) > 0 {
-					rat, _ := ratioHigh.Float64()
-					fmt.Printf("%v**%v=%v ~~ %v**%v=%v (%.6f%%)\n",
-						base1, power1, big1,
-						base2, power2High, big2High,
-						100*(rat-1),
-					)
+				for _, power1 := range powers {
+					n++
+					if n%nThreads != id {
+						continue
+					}
+
+					big1 := pow(base1, power1)
+					log2 := math.Log(float64(base2.Int64()))
+
+					power2 := float64(power1.Int64()) * log1 / log2
+					power2Low := big.NewInt(int64(math.Floor(power2)))
+					power2High := big.NewInt(int64(math.Ceil(power2)))
+
+					big2Low := pow(base2, power2Low)
+					big2High := pow(base2, power2High)
+
+					if verbose {
+						fmt.Printf("%v**%v (%v) < %v**%v (%v) < %v**%v (%v)\n",
+							base2, power2Low, big2Low,
+							base1, power1, big1,
+							base2, power2High, big2High,
+						)
+					}
+
+					ratioLow := big.NewRat(0, 1).SetFrac(big1, big2Low)
+					ratioHigh := big.NewRat(0, 1).SetFrac(big1, big2High)
+
+					if ratioLow.Cmp(lowTolerance) < 0 {
+						rat, _ := ratioLow.Float64()
+						fmt.Printf("%v**%v=%v ~~ %v**%v=%v (%.6f%%)\n",
+							base1, power1, big1,
+							base2, power2Low, big2Low,
+							100*(rat-1),
+						)
+					}
+
+					if ratioHigh.Cmp(highTolerance) > 0 {
+						rat, _ := ratioHigh.Float64()
+						fmt.Printf("%v**%v=%v ~~ %v**%v=%v (%.6f%%)\n",
+							base1, power1, big1,
+							base2, power2High, big2High,
+							100*(rat-1),
+						)
+					}
 				}
 			}
 		}
+
+		if debug {
+			fmt.Printf("Cache: %d/%d=%.1f%%\n", hits, misses, 100*float64(hits)/(float64(hits+misses)))
+		}
 	}
 
-	if debug {
-		fmt.Printf("Cache: %d/%d=%.1f%%\n", hits, misses, 100*float64(hits)/(float64(hits+misses)))
+	for i := 0; i < nThreads; i++ {
+		wg.Add(1)
+		go compute(i)
 	}
+	wg.Wait()
 }
